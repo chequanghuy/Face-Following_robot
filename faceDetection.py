bf654c259a03e215fb89e8f731925e32f31e7f2d
Void PhongEnder <43289563+phonghongs@users.noreply.github.com>
2019-07-15 13:37:11 +0700

    Update

--- faceDetection.py
+++ faceDetection.py
@@ -5,44 +5,49 @@ import numpy as np
 import cv2
 
 # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier('cascade/haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)                                       #read Camera
 cap.set(3,640)                                                  # set Width
 cap.set(4,480)                                                  # set Height
 
-center_of_Face = [0,0]                                                  # khai báo mảng chứa tọa độ tâm khuôn mặt đang theo
+Center = (320, 240)
+center_of_Face = (0,0)                                          # khai báo mảng chứa tọa độ tâm khuôn mặt đang theo
 
 while True: 
     ret, img = cap.read()                                       # đọc Camera
-#_________________________________  Face recognition____________________________________#
+    # img = cv2.flip(img, -1)                                     # Xoay Camera
+#_________________________________  Face recognition ____________________________________#
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                
 
-    faces = faceCascade.detectMultiScale(
-        gray ,
-        scaleFactor=1.2 ,
-        minNeighbors=5 ,     
-        minSize=(20, 20)
-    )
-#_______________________________________________________________________________________#
+    # faces = faceCascade.detectMultiScale(
+    #     gray ,
+    #     scaleFactor=1.2 ,
+    #     minNeighbors=5 ,     
+    #     minSize=(20, 20)
+    # )
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+#________________________________________________________________________________________#
     minz = 99999
-    face_centers = ((0,0), (0,0))
-    face = [0,0]
-    for (x,y,w,h) in faces:
-        a = [x + w/2, y + h/2]
-        b = distance(center_of_Face, a)
-        if (b < minz):
-            minz = b
-            face_centers = ((x,y),(x+w,y+h))
-            face = a
-        # cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        # print(x,y,w,h) 
-    cv2.rectangle(img,face_centers[0],face_centers[1],(255,0,0),2)
-    center_of_Face = face
+    if faces == ():
+        cv2.imshow('video',img)
+    else:
+        face_centers = ((0,0), (0,0))
+        face = (0,0)
+        for (x,y,w,h) in faces:
+            a = (int(x + w/2), int(y + h/2))
+            b = distance(center_of_Face, a)
+            if (b < minz):
+                minz = b
+                face_centers = ((x,y), (x+w,y+h))
+                face = a
 
-    cv2.line(img, )
-    cv2.imshow('video',img)
-    print(img.shape) #240 - 320
+        cv2.rectangle(img,face_centers[0],face_centers[1],(255,0,0),2)
+        center_of_Face = face
+        cv2.line(img, center_of_Face, Center, (0, 255, 0), 5)
+        cv2.imshow('video',img)
+
     k = cv2.waitKey(30) & 0xff
     if k == 27: # press 'ESC' to quit
         break
